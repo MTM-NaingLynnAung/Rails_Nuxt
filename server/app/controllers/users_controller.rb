f96f907341edb595be4277c29eed91f1456ad960
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-12-02 17:04:41 +0630

    fix cors error

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    @user.id = session[:user_id]
+    @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user
-      @user.update(name: params[:name], email: params[:email])
+    if @user.update(user_params)
       render json: @user, status: 200
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -41,7 +40,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find_by(id: params[:id])
     end
-    # def user_params
-    #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    # end
+    def user_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
 end
